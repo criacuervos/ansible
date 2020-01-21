9467f816d559017826aa889e66f1309f453d18fd
v <vanessareigosa@gmail.com>
2020-01-20 16:17:00 -0800

    created new graphics layer on top of canvas where stars are rendered, so that constellation drawing happens underneath stars

--- public/sketch.js
+++ public/sketch.js
@@ -4,7 +4,9 @@ let bgColor;
 let cv;
 let x, y;
 let radius1, radius2;
+let color1;
 let starPositions = [];
+let pg;
 
 function setup() {
   socket = io.connect();
@@ -12,7 +14,10 @@ function setup() {
   cv = createCanvas(windowWidth, windowHeight)
   cv.position(0, 0);
   cv.style('z-index', '-1')
-  cv.background(15)
+  // cv.background(15)
+  color1 = color('#24243e');
+  var color2 = color('#302b63');
+  setGradient(0, 0, windowWidth, windowHeight, color1, color2, "Y");
   r = random(255);
   g = random(255);
   b = random(255);
@@ -26,8 +31,8 @@ function setup() {
     if(starData.length > 0 ){
       console.log("This is what a second or third client should see here")
       for (let {x, y, radius1, radius2} of starData[0]){
-        // console.log(x, y, radius1, radius2)
-        star(x, y, radius1, radius2, 5);
+        // star(x, y, radius1, radius2, 5);
+        starPositions.push({x, y, radius1, radius2})
       }
     } else if (starData.length === 0){
       console.log("This is first client code should go")
@@ -42,7 +47,8 @@ function setup() {
       socket.emit('star-states', starPositions)
     }
   })
-  // socket.on
+
+  pg = createGraphics(windowWidth, windowHeight)
 }
 
 function draw(){ 
@@ -51,15 +57,29 @@ function draw(){
   } else if (keyIsPressed && key == 'e'){
     drawEraser();
   }
-  // console.log(mouseX, mouseY)
+  //code underneath image will render on top, so all drawn code will render on top layer 
+  image(pg, 0, 0, windowWidth, windowHeight);
+  for (let star of starPositions){
+    noStroke()
+    this.star(star.x, star.y, star.radius1, star.radius2, 5);
+  }
+  makeMoon()
 
+}
+
+function makeMoon(){
+  noStroke();
+  fill(230,230,180);
+  ellipse(1200,200,120,120);
+  fill(color1);
+  ellipse(1210,200,110,120);
 }
 
 function star(x, y, radius1, radius2, npoints) {
   let angle = TWO_PI / npoints;
   let halfAngle = angle / 2.0;
   beginShape();
-  fill(255)
+  fill(255, 204, 0)
   for (let a = 0; a < TWO_PI; a += angle) {
     let sx = x + cos(a) * radius2;
     let sy = y + sin(a) * radius2;
@@ -67,7 +87,6 @@ function star(x, y, radius1, radius2, npoints) {
     sx = x + cos(a + halfAngle) * radius1;
     sy = y + sin(a + halfAngle) * radius1;
     vertex(sx, sy);
-    // noStroke()
   }
   endShape(CLOSE);
 }
@@ -118,6 +137,22 @@ function drawEraser(){
   socket.emit('mouse', data)
 }
 
-// function windowResized(){
-//   resizeCanvas(windowWidth, windowHeight);
-// }
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+  if (axis == "Y") {  // Top to bottom gradient
+    for (let i = y; i <= y+h; i++) {
+      var inter = map(i, y, y+h, 0, 1);
+      var c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x+w, i);
+    }
+  }  
+  else if (axis == "X") {  // Left to right gradient
+    for (let j = x; j <= x+w; j++) {
+      var inter2 = map(j, x, x+w, 0, 1);
+      var d = lerpColor(c1, c2, inter2);
+      stroke(d);
+      line(j, y, j, y+h);
+    }
+  }
+}
